52372acd0040dd4a7bf33d6d5dd5a63ec91e851a
Hidan1992 <154265690+Hidan1992@users.noreply.github.com>
2023-12-28 16:28:43 +0100

    Up

--- ULN2003/ULN2003.py
+++ ULN2003/ULN2003.py
@@ -8,11 +8,11 @@ in3 = 27
 in4 = 22
 
 # careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
-step_sleep = 0.1
+step_sleep = 0.001
 
-step_count = 512 # 5.625*(1/64) per step, 4096 steps is 360°
+step_count = 4096 # 5.625*(1/64) per step, 4096 steps is 360°
 
-direction = True # True for clockwise, False for counter-clockwise
+direction = False # True for clockwise, False for counter-clockwise
 
 # defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
 step_sequence = [[1,0,0,1],
